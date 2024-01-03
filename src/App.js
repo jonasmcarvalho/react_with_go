7c76518ea6d91250f6b02587c59814d7edd0bdc2
Jonas Carvalho <jonasmcarvalho@outlook.com>
2024-01-02 21:19:58 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,145 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import './App.css'
+import Input from "./Input";
 
-function App() {
+function HelloWorld(props) {
+  const [isTrue, setIsTrue] = useState(true)
+  const [crowd, setCrowd] = useState([])
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [dob, setDob] = useState('')
+
+  const toggleTrue = () => {
+    if(isTrue) {
+      setIsTrue(false)
+      return
+    }
+    setIsTrue(true)
+  }
+
+  useEffect(() => {
+    console.log('useEffect fired!')
+
+    let people = [
+      {
+        id: 1,
+        firstName: 'Mary',
+        lastName: 'Jones',
+        dob: '1997-05-02'
+      },
+      {
+        id: 2,
+        firstName: 'Jack',
+        lastName: 'Smith',
+        dob: '1999-02-04'
+      }
+    ]
+    setCrowd(people)
+  }, [])
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    if(lastName !== '') {
+      addPesron(firstName, lastName, dob)
+    }
+  }
+
+  const addPesron = (newFirst, newLast, newDOB) => {
+    let newPerson = {id: crowd.length + 1,
+      firstName: newFirst,
+      lastName: newLast,
+      dob: newDOB
+    }
+
+    const newList = crowd.concat(newPerson)
+
+    const sorted = newList.sort((a, b) => {
+      if (a.lastName < b.lastName) {
+        return -1
+      } else if (a.lastName > b.lastName) {
+        return 1
+      }
+      return 0
+    })
+
+    setCrowd(sorted)
+    setFirstName('')
+    setLastName('')
+    setDob('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Fragment>
+      <hr />
+      <h1 className="h1-green">{props.msg}</h1>
+      <hr />
+
+      {isTrue &&
+        <Fragment>
+          <p>The current value of isTrue os true</p>
+          <hr />
+        </Fragment>
+      }
+      <hr />
+      {isTrue
+      ? <p>Is true</p>
+      : <p>Is false</p>
+      }
+      <hr />
+      <a href="#!" className="btn btn-outline-secondary" onClick={toggleTrue}>Toglle isTrue</a>
+
+      <form autoComplete="off" onSubmit={handleSubmit}>
+        <div className="md-3">
+          <label className="form-label" htmlFor="first-name">First Name</label>
+          <input 
+            type="text"
+            name="first-name"
+            id="first-name"
+            autoComplete="first-name-new"
+            className="form-control"
+            onChange={(event) => setFirstName(event.target.value)}
+          />
+        </div>
+
+        <Input
+          title="Last Name"
+          type="text"
+          name="last-name"
+          autoComplete="last-name-new"
+          className="form-control"
+          onChange={(event) => setLastName(event.target.value)}
+        ></Input>
+
+        <Input
+          title="Date of Birth"
+          type="date"
+          name="dob"
+          autoComplete="dob-new"
+          className="form-control"
+          onChange={(event) => setDob(event.target.value)}
+        ></Input>
+
+        <input type="submit" value="Submit" className="btn btn-primary"></input>
+      </form>
+
+      <div>
+        First Name: {firstName} <br />
+        Last Name: {lastName} <br />
+        DOB Name: {dob} <br />
+      </div>
+
+      <hr />
+      <h3>People</h3>
+      <ul className="list-group">
+        {crowd.map((m) => (
+          <li key={m.id} className="list-group-item">{m.firstName} {m.lastName}</li>
+        ))}
+      </ul>
+    </Fragment>
+  )
 }
 
-export default App;
+export default HelloWorld;
